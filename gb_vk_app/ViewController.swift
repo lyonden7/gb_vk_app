b061a30d7db2454f7e91f2e249391ee8106cab74
Denis Vasilev <lyon_den@mail.ru>
2022-01-31 01:30:47 +0300

    Initial Commit

---
+++ gb_vk_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gb_vk_app
+//
+//  Created by Денис Васильев on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
